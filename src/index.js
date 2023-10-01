77a551770f33321b16022e3e60673a56aaecfd0a
X01SERYO <sergio.s16a@hotmail.com>
2023-10-01 12:34:18 -0500

    feat: desing the login

--- src/index.js
+++ src/index.js
@@ -4,14 +4,28 @@ import { App } from './App';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#005257',
+    },
+    secondary: {
+      main: '#FC5400',
+    },
+  },
+});
+
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
